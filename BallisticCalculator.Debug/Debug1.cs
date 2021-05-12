a678bb3ebcfc8154532fa80bf86dd9583c8c99b3
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2021-05-12 15:47:33 -0400

    - add 4 test reticles

--- BallisticCalculator.Debug/Debug1.cs
+++ BallisticCalculator.Debug/Debug1.cs
@@ -9,12 +9,12 @@ namespace BallisticCalculator.Debug
 {
     internal static class Debug1
     {
-        public static void Do(String[] _)
+        internal static TrajectoryPoint[] M855(bool hasWind, Measurement<DistanceUnit> step)
         {
             //define M855 projectile out of 20 inch barrel
             var ammo = new Ammunition(
                 weight: new Measurement<WeightUnit>(62, WeightUnit.Grain),
-                ballisticCoefficient: new BallisticCoefficient(0.304, DragTableId.G1),
+                ballisticCoefficient: new BallisticCoefficient(0.205, DragTableId.G1),
                 muzzleVelocity: new Measurement<VelocityUnit>(3095, VelocityUnit.FeetPerSecond),
                 bulletDiameter: new Measurement<DistanceUnit>(0.224, DistanceUnit.Inch),
                 bulletLength: new Measurement<DistanceUnit>(0.9, DistanceUnit.Inch));
@@ -43,9 +43,9 @@ namespace BallisticCalculator.Debug
 
             //define atmosphere
             var atmosphere = new Atmosphere(
-                altitude: new Measurement<DistanceUnit>(100, DistanceUnit.Foot),
+                altitude: new Measurement<DistanceUnit>(0, DistanceUnit.Foot),
                 pressure: new Measurement<PressureUnit>(29.92, PressureUnit.InchesOfMercury),
-                pressureAtSeaLevel: true,
+                pressureAtSeaLevel: false,
                 temperature: new Measurement<TemperatureUnit>(59, TemperatureUnit.Fahrenheit),
                 humidity: 0.78);
 
@@ -55,14 +55,14 @@ namespace BallisticCalculator.Debug
             var shot = new ShotParameters()
             {
                 MaximumDistance = new Measurement<DistanceUnit>(1000, DistanceUnit.Yard),
-                Step = new Measurement<DistanceUnit>(50, DistanceUnit.Yard),
+                Step = step,
                 //calculate sight angle for the specified zero distance
                 SightAngle = calc.SightAngle(ammo, rifle, atmosphere)
             };
 
             //define winds
 
-            Wind[] wind = new Wind[2]
+            Wind[] wind = hasWind ? new Wind[2]
             {
                 new Wind()
                 {
@@ -75,10 +75,87 @@ namespace BallisticCalculator.Debug
                     Direction = new Measurement<AngularUnit>(15, AngularUnit.Degree),
                     Velocity = new Measurement<VelocityUnit>(5, VelocityUnit.MilesPerHour),
                 }
-            };
+            } : null;
 
             //calculate trajectory
-            var trajectory = calc.Calculate(ammo, rifle, atmosphere, shot, wind);
+            return calc.Calculate(ammo, rifle, atmosphere, shot, wind);
+        }
+
+        internal static TrajectoryPoint[] M193(bool hasWind, Measurement<DistanceUnit> step)
+        {
+            //define M193 projectile out of 20 inch barrel
+            var ammo = new Ammunition(
+                weight: new Measurement<WeightUnit>(55, WeightUnit.Grain),
+                ballisticCoefficient: new BallisticCoefficient(0.202, DragTableId.G1),
+                muzzleVelocity: new Measurement<VelocityUnit>(3240, VelocityUnit.FeetPerSecond),
+                bulletDiameter: new Measurement<DistanceUnit>(0.224, DistanceUnit.Inch),
+                bulletLength: new Measurement<DistanceUnit>(0.76, DistanceUnit.Inch));
+
+            //define ACOG scope
+            var sight = new Sight(
+                sightHeight: new Measurement<DistanceUnit>(3.5, DistanceUnit.Inch),
+                verticalClick: new Measurement<AngularUnit>(1.0 / 3.0, AngularUnit.InchesPer100Yards),
+                horizontalClick: new Measurement<AngularUnit>(1.0 / 3.0, AngularUnit.InchesPer100Yards)
+                );
+
+            //M16 rifling
+            var rifling = new Rifling(
+                riflingStep: new Measurement<DistanceUnit>(12, DistanceUnit.Inch),
+                direction: TwistDirection.Right);
+
+            //standard 100 yard ACOG zeroing
+            var zero = new ZeroingParameters(
+                distance: new Measurement<DistanceUnit>(100, DistanceUnit.Yard),
+                ammunition: null,
+                atmosphere: null
+                );
+
+            //define rifle by sight, zeroing and rifling parameters
+            var rifle = new Rifle(sight: sight, zero: zero, rifling: rifling);
+
+            //define atmosphere
+            var atmosphere = new Atmosphere(
+                altitude: new Measurement<DistanceUnit>(0, DistanceUnit.Foot),
+                pressure: new Measurement<PressureUnit>(29.92, PressureUnit.InchesOfMercury),
+                pressureAtSeaLevel: false,
+                temperature: new Measurement<TemperatureUnit>(59, TemperatureUnit.Fahrenheit),
+                humidity: 0.78);
+
+            var calc = new TrajectoryCalculator();
+
+            //shot parameters
+            var shot = new ShotParameters()
+            {
+                MaximumDistance = new Measurement<DistanceUnit>(1000, DistanceUnit.Yard),
+                Step = step,
+                //calculate sight angle for the specified zero distance
+                SightAngle = calc.SightAngle(ammo, rifle, atmosphere)
+            };
+
+            //define winds
+
+            Wind[] wind = hasWind ? new Wind[2]
+            {
+                new Wind()
+                {
+                    Direction = new Measurement<AngularUnit>(45, AngularUnit.Degree),
+                    Velocity = new Measurement<VelocityUnit>(10, VelocityUnit.MilesPerHour),
+                    MaximumRange = new Measurement<DistanceUnit>(500, DistanceUnit.Yard),
+                },
+                new Wind()
+                {
+                    Direction = new Measurement<AngularUnit>(15, AngularUnit.Degree),
+                    Velocity = new Measurement<VelocityUnit>(5, VelocityUnit.MilesPerHour),
+                }
+            } : null;
+
+            //calculate trajectory
+            return calc.Calculate(ammo, rifle, atmosphere, shot, wind);
+        }
+
+        public static void Do(String[] _)
+        {
+            var trajectory = M855(true, DistanceUnit.Yard.New(50));
 
             //print trajectory
             foreach (var point in trajectory)
---
+++ BallisticCalculator.Debug/Debug3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using BallisticCalculator.Reticle.Data;
+using BallisticCalculator.Reticle.Draw;
+using BallisticCalculator.Serialization;
+using Gehtsoft.Measurements;
+using Svg;
+
+namespace BallisticCalculator.Debug
+{
+    public static class Debug3
+    {
+        private static void Draw(ReticleDefinition reticle, string baseName, TrajectoryPoint[] trajectory)
+        {
+            double svgWidth = 10;
+            double svgHeight = Math.Round(reticle.Size.Y / reticle.Size.X * svgWidth, 2);
+
+
+            var canvas = SvgCanvasFactory.Create("reticle", $"{svgWidth}in", $"{svgHeight}in");
+            var controller = new ReticleDrawController(reticle, canvas);
+            controller.DrawReticle();
+            controller.DrawBulletDropCompensator(trajectory, DistanceUnit.Yard.New(100), false, DistanceUnit.Yard, "black");
+            var svg = SvgCanvasFactory.ToSvg(canvas);
+
+            int pngWidth = 1024;
+            int pngHeight = (int)((reticle.Size.Y / reticle.Size.X) * pngWidth);
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(svg);
+            var svgDocument = SvgDocument.Open(xmlDocument);
+            var bm = svgDocument.Draw(pngWidth, pngHeight);
+            var bm1 = new Bitmap(pngWidth, pngHeight);
+            Graphics g = Graphics.FromImage(bm1);
+            g.FillRectangle(Brushes.White, 0, 0, pngWidth, pngHeight);
+            g.DrawImage(bm, 0, 0);
+            bm1.Save($"{baseName}.png", ImageFormat.Png);
+            xmlDocument.Save($"{baseName}.svg");
+
+            xmlDocument = new XmlDocument();
+            BallisticXmlSerializer serializer = new BallisticXmlSerializer(xmlDocument);
+            xmlDocument.AppendChild(serializer.Serialize(reticle));
+            xmlDocument.Save($"{baseName}.reticle");
+        }
+
+        public static void Do(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load($@"..\..\..\..\data\reticle\{args[0]}.reticle");
+            BallisticXmlDeserializer ds = new BallisticXmlDeserializer();
+            var reticle = ds.Deserialize<ReticleDefinition>(document.DocumentElement);
+
+            TrajectoryPoint[] trajectoryPoints;
+
+            if (args.Length > 1 && args[1] == "55")
+                trajectoryPoints = Debug1.M193(false, DistanceUnit.Yard.New(10));
+            else
+                trajectoryPoints = Debug1.M855(false, DistanceUnit.Yard.New(10));
+
+            Draw(reticle, args[0], trajectoryPoints);
+        }
+    }
+}
