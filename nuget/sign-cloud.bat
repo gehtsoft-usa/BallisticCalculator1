a6f8a326503fe529f80f28e962de227a569e0655
Nikolay Gekht <nikolay.gekht@gmail.com>
2026-07-27 13:34:11 -0400

    small updates in harness

--- nuget/sign-cloud.bat
+++ nuget/sign-cloud.bat
@@ -1,2 +1,2 @@
 rem @ECHO off
-forfiles /m *.nupkg /c "cmd /c nuget sign -CertificateFingerprint %digicert-fingerprint% -Timestamper http://timestamp.digicert.com @path"
+forfiles /m *.nupkg /c "cmd /c nuget sign -CertificateFingerprint %DIGICERT_CERTIFICATE% -Timestamper http://timestamp.digicert.com @path"
